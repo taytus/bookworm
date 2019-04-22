1a45dd9097cbd4d7f42675747517e1a7aecc128b
Roberto Inetti <copertus@gmail.com>
2019-04-22 09:40:58 -0500

    added new method to the URL package

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -4,17 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-use ROBOAMP\Strings;
+use ROBOAMP\URL;
+
 class WelcomeController extends Controller{
 
 
     public function main(){
 
-        $str=new Strings();
+        $res='https://amp.mansfield-dentalcare.com?id=8fc33172-1b47-4eb3-95b9-92e3d4007912&page=https%3A%2F%2Fwww.mansfield-dentalcare.com%2F';
 
-        dd($str::get_string_between('<nothing>','<','>'));
+        $res=URL::get_property_info_from_roboamp_install_code($res);
 
-
+        dd($res);
     }
     public function lol(){
         dd("LOL");
