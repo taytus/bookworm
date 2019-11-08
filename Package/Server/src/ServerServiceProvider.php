d790cc9e13a71f2a3e99b47295efc6b1c2510ef9
Roberto Inetti <copertus@gmail.com>
2019-11-07 23:22:36 -0600

    Server has been added

---
+++ Package/Server/src/ServerServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+namespace ROBOAMP\Server;
+use Illuminate\Support\ServiceProvider;
+
+
+class ServerServiceProvider extends ServiceProvider{
+
+    public function boot(){
+
+    }
+    public function register(){
+        $this->app->singleton(Server::class, function () {
+            return new Server();
+        });
+        $this->app->alias(Server::class, 'Server');
+
+    }
+
+}
