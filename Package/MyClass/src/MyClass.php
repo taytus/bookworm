0c3a4f4e4da0125f84fb0d0272cc7ee1a4add7bf
Roberto Inetti <copertus@gmail.com>
2019-11-07 23:10:09 -0600

    MyClass is back

---
+++ Package/MyClass/src/MyClass.php
@@ -0,0 +1,39 @@
+<?php
+namespace ROBOAMP;
+
+use \ReflectionMethod;
+
+
+class MyClass{
+
+
+    //create_class_from_string("Taytus","lol") will return "\App\MyClasses\lol\CliTaytus"
+    public static function create_class_from_string($string,$path="Cli",$prefix="Cli",$output){
+
+        $my_class= "\\App\\MyClasses\\".$path."\\".$prefix.ucfirst($string);
+        $my_class= new $my_class($output);
+        return $my_class;
+    }
+
+    //helper to call any class/method without worrying if it is static or not
+    public static function call_method($class,$method,$params=null){
+
+        $tmp_params=$params;
+        if(!is_array($params)){
+            $params=array();
+            $params[0]=$tmp_params;
+        }
+
+        if(!class_exists($class)){
+            dd ("CLASS DOESN'T EXIST","Class: ".$class,"Method:  ".$method);
+        }
+        if(method_exists($class,$method)){
+            $MethodChecker = new ReflectionMethod($class,$method);
+            $res = $MethodChecker->invokeArgs(new $class($params), $params);
+            return $res;
+        }else{
+            echo ("METHOD DOESN'T EXIST\nClass\t".$class."\nMethod\t".$method."\n");
+        }
+    }
+
+}
---
+++ Package/MyClass/src/MyClassServiceProvider.php
@@ -0,0 +1,22 @@
+<?php
+namespace ROBOAMP\MyClass;
+use Illuminate\Support\ServiceProvider;
+
+use ROBOAMP\Axton;
+use ROBOAMP\MyClass;
+
+class MyClassServiceProvider extends ServiceProvider{
+    public function boot(){
+
+    }
+    public function register(){
+        $this->app->singleton(MyClass::class, function () {
+
+
+            return new MyClass();
+        });
+
+        $this->app->alias(MyClass::class, 'MyClass');
+    }
+
+}
