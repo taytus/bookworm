0c1ac82eea128410b80c22f179ffc511903742fc
Roberto Inetti <copertus@gmail.com>
2019-11-18 04:46:23 -0600

    auto update for package Directory

--- Package/Directory/tests/Feature/Test.php
+++ Package/Directory/tests/Feature/Test.php
@@ -10,16 +10,23 @@ class Test extends TestCase
      */
     public function testBasicTest(){
 
-
+        $this->create_directory();
 
 
 
     }
 
     public function create_directory(){
+        $directory_path=base_path('test');
         $directory_class=new Directory_test();
-        $directory_path=base_path('/test');
-        $this->assertFileExists($directory_path);
+
+
+        if($this->assertDirectoryNotExists($directory_path)){
+            $directory_class->create_folder($directory_path);
+            $this->assertDirectoryExists($directory_path);
+
+        }
+
 
     }
 
