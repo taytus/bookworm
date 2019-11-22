2c389db698d58763411c6bbaaa34488550ed6ddb
Roberto Inetti <copertus@gmail.com>
2019-11-22 14:52:54 -0600

    auto update for package CLI

--- Package/CLI/src/Debug.php
+++ Package/CLI/src/Debug.php
@@ -9,11 +9,10 @@ class Debug   {
 
     public static function log($error_message){
         $res=debug_backtrace();
-        $cli=$res[1]["object"]->cli;
         $cliStyle=new CliStyle();
         //the [0] position is the one calling this method, the one alarming of an error
         //[1] is the one calling [0]
-        $error_message="Error: ".$error_message."\t\t\t\t\t\t\t\t\t\t";
+        $error_message="Error: ".$error_message."\t\t\t\t\t\t\t\t\t\t\t";
         $triggered_from="Error on Method: ".$res[1]['function']."\t | Class: ".$res[1]["class"]."\t\t\t\t\t\t\t\t\t";
         $called_from= "Called from ".$res[1]['file']." \t | Line: ".$res[1]["line"]."\t\t\t\t\t\t";
 
