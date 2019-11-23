f83507a1b2d79931abd2ea94fcc03a0d789bea96
Roberto Inetti <copertus@gmail.com>
2019-11-23 02:28:06 -0600

    auto update for package CLI

--- Package/CLI/src/Debug.php
+++ Package/CLI/src/Debug.php
@@ -31,7 +31,6 @@ class Debug   {
         $message=$message.$tabs;
         $triggered_from="Message triggered on Method: ".$res[1]['function']."\t | Class: ".$res[1]["class"];
         $tabs=$string_class->get_total_tabs($triggered_from);
-        dd("Tabs from second message = ".$tabs,strlen($triggered_from),19-strlen($triggered_from));
         $triggered_from=$triggered_from.$tabs;
 
         $cliStyle->log_message($message);
