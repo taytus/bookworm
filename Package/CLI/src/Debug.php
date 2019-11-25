8ab4ee52214ced1adbe6a177283c26e8e8e70133
Roberto Inetti <copertus@gmail.com>
2019-11-25 05:16:21 -0600

    auto update for package CLI

--- Package/CLI/src/Debug.php
+++ Package/CLI/src/Debug.php
@@ -14,7 +14,8 @@ class Debug   {
         //the [0] position is the one calling this method, the one alarming of an error
         //[1] is the one calling [0]
         $error_message="Error: ".$error_message."\t\t\t\t\t\t\t\t\t\t\t";
-        $triggered_from="Error on Method: ".$res[1]['function']."\t | Class: ".$res[1]["class"]."\t\t\t\t\t\t\t\t\t";
+        $class=(isset($res[1]['class'])? "| Class: ".$res[1]["class"]:"");
+        $triggered_from="Error on Method: ".$res[1]['function']."\t".$class."\t\t\t\t\t\t\t\t\t";
         $called_from= "Called from ".$res[1]['file']." \t | Line: ".$res[1]["line"]."\t\t\t\t\t\t";
 
         $cliStyle->error_message($error_message);
